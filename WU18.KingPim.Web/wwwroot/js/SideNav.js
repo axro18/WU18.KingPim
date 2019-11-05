5dd189dc6ad59937457196657bdcce77aa96842c
Robert Axelsson <axro18km@student.ju.se>
2019-11-05 10:22:26 +0100

    Försök till sidenavbar men fungerar inte ännu

---
+++ WU18.KingPim.Web/wwwroot/js/SideNav.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function() {
+    $("#sidebarCollapse").on("click",
+        function() {
+            $("#sidebar").toggleClass("active");
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        });
+});
